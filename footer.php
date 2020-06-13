edca58f9171ed2809760057d4ddb0eb9da7f1c60
Vasiliy <kkolyasev1228@gmail.com>
2020-06-13 13:56:56 +0300

    footer menu

--- footer.php
+++ footer.php
@@ -4,14 +4,11 @@
 
         <div class="twelve columns">
 
-            <ul class="footer-nav">
-                <li><a href="#">Home.</a></li>
-                <li><a href="#">Blog.</a></li>
-                <li><a href="#">Portfolio.</a></li>
-                <li><a href="#">About.</a></li>
-                <li><a href="#">Contact.</a></li>
-                <li><a href="#">Features.</a></li>
-            </ul>
+            <?wp_nav_menu([
+                'theme_location'  => 'footer',
+                'container'       => null,
+                'menu_class'      => 'footer-nav',
+            ]); ?>
 
             <ul class="footer-social">
                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
